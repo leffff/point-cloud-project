f2b93eab466fce08ba4a5da29a37176c7901adec
Lev Novitskiy <57654885+leffff@users.noreply.github.com>
2023-04-07 00:29:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-pip install requirements.txt
+pip install -r requirements.txt
 pip install torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-$1.13.1+$cu117.html
 pip install torch-geometric
